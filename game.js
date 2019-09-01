d5497943d9b328b9134f88c4589112f99cf3a4c3
Ida Stensrud <idast3@gmail.com>
2019-09-01 23:28:35 +0200

    game works

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 function boardGame() {
 
+
   function createTile() {
       var tile = document.createElement("div");
 
@@ -24,10 +25,47 @@ function boardGame() {
     allTiles[30].className = "win";
     allTiles[30].innerText = "magic stone";
 
+    allTiles[4].style.backgroundImage = "url('characters/crystal-icon.png')";
+    allTiles[4].style.backgroundSize = "30%";
+    allTiles[4].style.backgroundRepeat = "no-repeat";
+    allTiles[4].style.backgroundPosition= "center";
+
+    allTiles[8].style.backgroundImage = "url('characters/crystal-icon.png')";
+    allTiles[8].style.backgroundSize = "30%";
+    allTiles[8].style.backgroundRepeat = "no-repeat";
+    allTiles[8].style.backgroundPosition= "center";
+
+    allTiles[12].style.backgroundImage = "url('characters/crystal-icon.png')";
+    allTiles[12].style.backgroundSize = "30%";
+    allTiles[12].style.backgroundRepeat = "no-repeat";
+    allTiles[12].style.backgroundPosition= "center";
+
+    allTiles[16].style.backgroundImage = "url('characters/crystal-icon.png')";
+    allTiles[16].style.backgroundSize = "30%";
+    allTiles[16].style.backgroundRepeat = "no-repeat";
+    allTiles[16].style.backgroundPosition= "center";
+
+    allTiles[20].style.backgroundImage = "url('characters/crystal-icon.png')";
+    allTiles[20].style.backgroundSize = "30%";
+    allTiles[20].style.backgroundRepeat = "no-repeat";
+    allTiles[20].style.backgroundPosition= "center";
+
+    allTiles[24].style.backgroundImage = "url('characters/crystal-icon.png')";
+    allTiles[24].style.backgroundSize = "30%";
+    allTiles[24].style.backgroundRepeat = "no-repeat";
+    allTiles[24].style.backgroundPosition= "center";
+
+    allTiles[28].style.backgroundImage = "url('characters/crystal-icon.png')";
+    allTiles[28].style.backgroundSize = "30%";
+    allTiles[28].style.backgroundRepeat = "no-repeat";
+    allTiles[28].style.backgroundPosition= "center";
+
+
 
     var player1Character = localStorage.getItem("player1Character");
     var player2Character = localStorage.getItem("player2Character");
 
+    var winner = "";
 
     var token1 = document.createElement("span");
     var player1 = document.createTextNode(player1Character)
@@ -65,17 +103,115 @@ function boardGame() {
       number = Math.floor(Math.random() * 6) + 1;
       console.log("you got " + number);
 
-      // move token
-      position1 = position1 + number;
-      token1.removeChild(player1);
-      allTiles[position1].appendChild(token1);
-      token1.appendChild(player1);
+
+
+        // move token
+        position1 = position1 + number;
+        if (position1 < 30 ) {
+          token1.removeChild(player1);
+          allTiles[position1].appendChild(token1);
+          token1.appendChild(player1);
+        }
+
+
+        // traps
+        switch (position1) {
+          case 4:
+            setTimeout(function(){
+              alert("The weight of the world falls off your shoulders as this BLACK TOURMALINE drains away all the negative energy you have gathered. Take a light jump three steps forward.");
+              position1 = position1 + 3;
+              token1.removeChild(player1);
+              allTiles[position1].appendChild(token1);
+              token1.appendChild(player1);
+            }, 500);
+
+
+            break;
+
+          case 8:
+            setTimeout(function(){
+              alert("You discovered a ROSE QUARTZ, the crystal that makes your heart race and cheeks blush! Enjoy the romance as you float on a pink cloud five steps in the opposite direction.");
+              position1 = position1 - 5;
+              token1.removeChild(player1);
+              allTiles[position1].appendChild(token1);
+              token1.appendChild(player1);
+            }, 500);
+
+            break;
+
+          case 12:
+            setTimeout(function(){
+              alert("Ouch! You ran into a powerful MALACHITE that caused you a really bad headache. Take six steps backwards to feel better.")
+              position1 = position1 - 6;
+              token1.removeChild(player1);
+              allTiles[position1].appendChild(token1);
+              token1.appendChild(player1);
+            }, 500);
+
+
+            break;
+
+          case 16:
+            setTimeout(function(){
+              alert("This protective BLACK OBSIDIAN helps you brace yourself because winter is coming. Take five confident steps forward.")
+              position1 = position1 + 5;
+              token1.removeChild(player1);
+              allTiles[position1].appendChild(token1);
+              token1.appendChild(player1);
+            }, 500);
+
+            break;
+
+          case 20:
+            setTimeout(function(){
+              alert("You could swear you just spotted a sparkly PYRITE but it turns out it was just a regular BORING GREY STONE. So sure of yourself though, you take three steps backwards to look one more time.")
+              position1 = position1 - 3;
+              token1.removeChild(player1);
+              allTiles[position1].appendChild(token1);
+              token1.appendChild(player1);
+            }, 500);
+
+            break;
+
+          case 24:
+            setTimeout(function(){
+              alert("You just picked up a PURPLE AMETHYST which radiates so much calm energy that you fall asleep on the ground and roll two steps back. Sweet dreams!")
+              position1 = position1 - 2;
+              token1.removeChild(player1);
+              allTiles[position1].appendChild(token1);
+              token1.appendChild(player1);
+            }, 500);
+
+
+            break;
+
+          case 28:
+            setTimeout(function(){
+              alert("You just picked up a RAINBOW FLOURITE so powerful that it opens a portal gate in front of you. Curious as you are, you take the stone through the portal and end up five steps behind.")
+              position1 = position1 - 5;
+              token1.removeChild(player1);
+              allTiles[position1].appendChild(token1);
+              token1.appendChild(player1);
+            }, 500);
+
+            break;
+          default:
+
+        }
+
+
+
+      if (position1 > 29) {
+        localStorage.setItem("Winner", "Player 1");
+        winner = player1Character;
+        window.open("win.html");
+      }
 
 
     }, false); // END of PLAYER 1
 
 
-    // PLAYER 2 
+    // PLAYER 2
     player2Dice.addEventListener('click', function() {
 
       // edit this to match number 6, if number = less than 6
@@ -91,14 +227,113 @@ function boardGame() {
       number = Math.floor(Math.random() * 6) + 1;
       console.log("you got " + number);
 
-      // move token
-      position2 = position2 + number;
-      token2.removeChild(player2);
-      allTiles[position2].appendChild(token2);
-      token2.appendChild(player2);
 
+
+        // move token
+        position2 = position2 + number;
+        if (position2 < 30) {
+          token2.removeChild(player2);
+          allTiles[position2].appendChild(token2);
+          token2.appendChild(player2);
+        }
+
+
+        // traps
+        switch (position2) {
+          case 4:
+            setTimeout(function(){
+              alert("The weight of the world falls off your shoulders as this BLACK TOURMALINE drains away all the negative energy you have gathered. Take a light jump three steps forward.");
+              position2 = position2 + 3;
+              token2.removeChild(player2);
+              allTiles[position2].appendChild(token2);
+              token2.appendChild(player2);
+            }, 500);
+
+
+            break;
+
+          case 8:
+            setTimeout(function(){
+              alert("You discovered a ROSE QUARTZ, the crystal that makes your heart race and cheeks blush! Enjoy the romance as you float on a pink cloud five steps in the opposite direction.");
+              position2 = position2 - 5;
+              token2.removeChild(player2);
+              allTiles[position2].appendChild(token2);
+              token2.appendChild(player2);
+            }, 500);
+
+            break;
+
+          case 12:
+            setTimeout(function(){
+              alert("Ouch! You ran into a powerful MALACHITE that caused you a really bad headache. Take six steps backwards to feel better.")
+              position2 = position2 - 6;
+              token2.removeChild(player2);
+              allTiles[position2].appendChild(token2);
+              token2.appendChild(player2);
+            }, 500);
+
+
+            break;
+
+          case 16:
+            setTimeout(function(){
+              alert("This protective BLACK OBSIDIAN helps you brace yourself because winter is coming. Take five confident steps forward.")
+              position2 = position2 + 5;
+              token2.removeChild(player2);
+              allTiles[position2].appendChild(token2);
+              token2.appendChild(player2);
+            }, 500);
+
+            break;
+
+          case 20:
+            setTimeout(function(){
+              alert("You could swear you just spotted a sparkly PYRITE but it turns out it was just a regular BORING GREY STONE. So sure of yourself though, you take three steps backwards to look one more time.")
+              position2 = position2 - 3;
+              token2.removeChild(player2);
+              allTiles[position2].appendChild(token2);
+              token2.appendChild(player2);
+            }, 500);
+
+            break;
+
+          case 24:
+            setTimeout(function(){
+              alert("You just picked up a PURPLE AMETHYST which radiates so much calm energy that you fall asleep on the ground and roll two steps back. Sweet dreams!")
+              position2 = position2 - 2;
+              token2.removeChild(player2);
+              allTiles[position2].appendChild(token2);
+              token2.appendChild(player2);
+            }, 500);
+
+
+            break;
+
+          case 28:
+            setTimeout(function(){
+              alert("You just picked up a RAINBOW FLOURITE so powerful that it opens a portal gate in front of you. Curious as you are, you take the stone through the portal and end up five steps behind.")
+              position2 = position2 - 5;
+              token2.removeChild(player2);
+              allTiles[position2].appendChild(token2);
+              token2.appendChild(player2);
+            }, 500);
+
+            break;
+          default:
+
+        }
+
+
+
+      if (position2 > 29) {
+        localStorage.setItem("Winner", "Player 2");
+        winner = player2Character;
+        window.open("win.html");
+      }
 
     }, false); // END of PLAYER 2
+
+
 
   }
 
---
+++ win.js
@@ -0,0 +1,25 @@
+var winner = localStorage.getItem("Winner");
+console.log(winner + " has won the game");
+
+if (winner === "Player 1") {
+  var character = localStorage.getItem("player1Character");
+  switch (character) {
+    case "JS":
+      //congratulate Jon Snow for finding the mighty stone
+      break;
+    case "KD":
+
+      break;
+    case "GW":
+
+      break;
+    case "TG":
+
+      break;
+    case "VT":
+
+      break;
+    default:
+
+  }
+}
